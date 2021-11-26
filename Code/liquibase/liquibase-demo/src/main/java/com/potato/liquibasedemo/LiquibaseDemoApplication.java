0e3c7f0172795c6550e3f7575ab994fb71ab79a1
Potato20522 <2052298297@qq.com>
2021-11-27 00:12:26 +0800

    Liquibase

---
+++ Code/liquibase/liquibase-demo/src/main/java/com/potato/liquibasedemo/LiquibaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.potato.liquibasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseDemoApplication.class, args);
+    }
+
+}
